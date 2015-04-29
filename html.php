4674fe22396b250fd63727c32a2b2f7e648ebbe5
Edwin Corzo <edwin11ramirez@gmail.com>
2015-04-29 11:59:09 -0700

    had an error trying to do it

---
+++ html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edwins Quiz2</title>
+</head>
+<body>
+	<div class="dan">
+		<button>
+			Click Me
+		</button>
+	</div>
+
+	<div class="iwao">
+		<button>
+			Click Me
+		</button>
+		
+	</div>
+
+	<?php
+	$( "p" ).on( "click",function() {
+	console.log("click")
+	});
+	?>
+
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+</body>
+</html>
